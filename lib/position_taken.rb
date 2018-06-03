de461f842a7377b4b7504785bfec17c2fbb0df83
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-03 18:37:09 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board = " " || "" || nil
+    return false
+  elsif board == "X" || "O"
+    return true
+  end
+
+
+end
